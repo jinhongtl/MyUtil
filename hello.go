6a3c469a3095f8d9fb4d4f0b98d3afade3b01e41
jinhong <jinhongtl@163.com>
2023-07-16 21:14:20 +0800

    update

--- hello.go
+++ hello.go
@@ -1,9 +1,15 @@
 package MyUtil
 
+import "fmt"
+
 func Hello() string {
 	return "Hello,World!"
 }
 
 func HelloWorld() string {
 	return "world"
+}
+
+func HelloV2(format string, s string) string {
+	return fmt.Sprintf(format, s)
 }
